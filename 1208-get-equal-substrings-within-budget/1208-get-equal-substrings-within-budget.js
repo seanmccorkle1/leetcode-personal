d75a0bbc5d8bc210b10577319ef33d5e62d39b2a
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2024-05-27 20:01:16 -0400

    Time: 77 ms (10.09%), Space: 50 MB (100.00%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.js
@@ -0,0 +1,15 @@
+const equalSubstring = (s, t, maxCost) => {
+  let max = 0;
+  for (let left = -1, right = 0; right < s.length; ++right) {
+    maxCost -= Math.abs(s.charCodeAt(right) - t.charCodeAt(right));
+    if (maxCost >= 0) {
+      right - left > max && (max = right - left);
+    } else {
+      while (maxCost < 0) {
+        ++left;
+        maxCost += Math.abs(s.charCodeAt(left) - t.charCodeAt(left));
+      }
+    }
+  }
+  return max;
+}
