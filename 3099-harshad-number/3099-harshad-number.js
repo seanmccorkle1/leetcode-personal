607d63c66c9ccbc75ad68be1be1d5ccbb19cee44
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2024-05-29 14:56:05 -0400

    Time: 63 ms (5.73%), Space: 48.6 MB (62.73%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.js
+++ 3099-harshad-number/3099-harshad-number.js
@@ -1,37 +1,30 @@
-var log = function (...args) {
-    // Do something with args, for example, logging them
 
-    // console.log(...[args].join("|"))
-
-    console.log([...args].join("|"))
-}
-
-log("log1", "log2")
-
-
-var sumOfTheDigitsOfHarshadNumber = function (x) {
-
+var sumOfTheDigitsOfHarshadNumber = function(x) {
+    
     let digit_sum = x.toString()
-        .split("").reduce(callback, 0)
+    .split("").reduce(callback,0)
+    
+        // .reduce((a,b)=>a + Number(b),0)
 
-    // .reduce((a,b)=>a + Number(b),0)
-
-    if (x % digit_sum == 0) {
+    if (x % digit_sum == 0){
         return digit_sum
     }
 
     return -1
 }
 
-// var callback = (a,b) => {
 
-function callback(a,b) { 
-    return a+Number(b)
+var callback=(a,b)=> a+Number(b)
 
-    // (a, b) => Number(a) + Number(b)
 
-}
+// function callback(a,b) { 
+//     return a+Number(b)
+    
+//     // (a, b) => Number(a) + Number(b)
+// }
 
-console.log(sumOfTheDigitsOfHarshadNumber(18), 9) 
+sumOfTheDigitsOfHarshadNumber(18) // should return 9, the sum of the digits since
 
-// should return 9, the sum of the digits since
+
+
+
