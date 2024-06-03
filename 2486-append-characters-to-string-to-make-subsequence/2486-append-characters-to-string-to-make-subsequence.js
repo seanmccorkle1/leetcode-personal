8933eee107e9c20bc80c2fbceb3f5244fd4bbc41
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2024-06-02 20:32:57 -0400

    Time: 62 ms (34.48%), Space: 51.9 MB (89.66%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.js
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.js
@@ -1,6 +1,6 @@
 var appendCharacters = function(string, substring) {
     let index2 =0
-    let len = substring.length
+    // let len = substring.length
     let num_of_letters_in_substring_that_match = index2
 
     for(let index=0;index<string.length;index++){
@@ -13,81 +13,14 @@ var appendCharacters = function(string, substring) {
             index2++
         }
 
-        if(len===index2) {
+        if(substring.length===index2) {
             return 0
         }
     }
 
 
-    return len - num_of_letters_in_substring_that_match
+    return substring.length - num_of_letters_in_substring_that_match
 }
 
-appendCharacters(s = "coaching", t = "coding")
+appendCharacters(string = "coaching", substring = "coding")
 
-
-
-function toHex(d) {
-    var r = d % 16;
-    if (d - r == 0) {
-        return toChar(r);
-    }
-    return toHex((d - r) / 16) + toChar(r);
-}
-
-function toChar(n) {
-    const alpha = "0123456789ABCDEF";
-    return alpha.charAt(n);
-}
-
-
-const regexp = /t(e)(st(\d?))/g;
-const str = 'test1test2';
-
-const array = [...str.matchAll(regexp)]
-
-console.log(array[0]);
-// Expected output: Array ["test1", "e", "st1", "1"]
-
-console.log(array[1]);
-// Expected output: Array ["test2", "e", "st2", "2"]
-
-function searchStringStuff(string) {
-
-    string = string.trim().replaceAll("||", "").replaceAll("OR OR ", "OR")
-    var string_without_quotes
-
-    const there_is_a_bunch_of_quote_marks = s.matchAll('"').length >= 4
-    const there_is_a_bunch_of_ORs = s.matchAll('OR').length >= 2
-
-    if (there_is_a_bunch_of_quote_marks) {
-        // string_without_quotes=
-    }
-
-    // if
-
-    s = '"' + s + '"'
-    console.log(s.split("|").join('"|"'))
-
-
-}
-
-
-// string_without_quotes
-
-var s = ""
-
-s = `("Nicaraguan"|"Nicaragua"|"contras"|"Nicaraguas"|"Nicaragua's"|"contras" AROUND(20) "civil"|"communist"|"communism"|"communists"|"socialism"|"revolutionaries"|"revolutionary"|"socialists"|"Revolution"|"socialist"|"war"|"revolutions")`
-
-let res = searchStringStuff(s)
-
-var s2 = ""
-s2 = '80s|eighties|1980|1980s|nineties|90s|nineties|1990s|20th'
-
-
-console.log(res)
-
-
-"nicaraguan" | "Nicaragua|Nicaraguas|Nicaragua's civil|communist|Revolution|socialist|war"
-
-
-s.split("|")
