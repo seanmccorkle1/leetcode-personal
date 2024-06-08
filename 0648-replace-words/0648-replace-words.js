6a3a5b09b894bb65d917d9f81fdcaeefdcad6d2b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2024-06-08 00:34:26 -0400

    Time: 1247 ms (8.72%), Space: 56.5 MB (99.59%) - LeetHub

--- 0648-replace-words/0648-replace-words.js
+++ 0648-replace-words/0648-replace-words.js
@@ -1,51 +1,17 @@
-class TrieNode {
-    constructor() {
-        this.children = {};
-        this.isEnd = false;
-    }
-}
-
-class Trie {
-    constructor() {
-        this.root = new TrieNode();
-    }
-    
-    insert(word) {
-        let node = this.root;
-        for (const char of word) {
-            if (!node.children[char]) {
-                node.children[char] = new TrieNode();
-            }
-            node = node.children[char];
-        }
-        node.isEnd = true;
-    }
-    
-    search(word) {
-        let node = this.root;
-        let prefix = '';
-        for (const char of word) {
-            if (!node.children[char]) {
-                return word;
-            }
-            prefix += char;
-            if (node.children[char].isEnd) {
-                return prefix;
-            }
-            node = node.children[char];
-        }
-        return word;
-    }
-}
-
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
 var replaceWords = function(dictionary, sentence) {
-    const trie = new Trie();
-    for (const word of dictionary) {
-        trie.insert(word);
-    }
-    
-    const words = sentence.split(' ');
-    const result = words.map(word => trie.search(word));
-    
-    return result.join(' ');
+    let wordArr = sentence.split(" ");
+    wordArr = wordArr.map(w => {
+        for(let i = 0; i <= dictionary.length - 1; i++) {
+            if (w.indexOf(dictionary[i]) === 0) {
+                w = dictionary[i];
+            }
+        }
+        return w;
+    })
+    return wordArr.reduce((str, word) => str += `${word} `, "").trim();
 };
