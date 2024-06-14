786d7cc3b8eb67f366afe73753d48553def5fbf7
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2024-06-13 23:50:56 -0400

    Time: 201 ms (38.98%), Space: 62.3 MB (37.29%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.js
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.js
@@ -1,19 +1,25 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var minIncrementForUnique = function (nums) {
-    // Sort the array first
-    nums.sort((a, b) => a - b);
 
+var minIncrementForUnique = function (nums) {
+    
+    // Sort the array first
+    nums.sort((a, b) => a - b)
+    
     let numTracker = 0;   // Tracks the next unique number that should be set.
     let minIncrement = 0; // Counts the total increments required.
-
+    
     for (let num of nums) {
-        numTracker = Math.max(numTracker, num);
+        // 1, 4
+        numTracker = Math.max(numTracker, num)
         minIncrement += numTracker - num;
-        numTracker++; // Increment the tracker for the next number.
+        
+        // Increment the tracker for the next number.
+        ++numTracker 
     }
 
+    // min to make it unique
     return minIncrement;
-};
+}
+
+console.log(minIncrementForUnique([1,2,2])  ,  1, "just 1 on the last one")
+
+// describe it 1)
